34d8834f0982b3526b5b04559a40402002f207df
MOUADHHAOUALA <mouad-fe@outlook.com>
2023-01-11 10:28:31 +0100

    4

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require("express");
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successfull");
+  });
+
+/*
 app.get("/", (req, res) => {
   res.status(200).send("hello toys app");
 });
+*/
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`app is fully running on ${port}` );
+  console.log(`app is fully running on ${port}`);
 });
